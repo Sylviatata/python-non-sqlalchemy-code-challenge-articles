beb267874cff01368bf5b1ecc7da38291ac0b416
Sylvia <sylvia.tata@student.moringaschool.com>
2024-07-10 16:17:10 +0300

    non SQL Alchemy code challnenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,15 +1,32 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
+        if not isinstance(title, str):
+            raise TypeError("Title must be a string")
         self.author = author
         self.magazine = magazine
-        self.title = title
-        
+        self._title = title  # Use a private attribute to store the title
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        raise AttributeError("Article title is immutable")
+
 class Author:
     def __init__(self, name):
         self.name = name
+        self._articles = []
+
+    def add_article(self, article):
+        self._articles.append(article)
 
     def articles(self):
-        pass
+        return self._articles
 
     def magazines(self):
         pass
